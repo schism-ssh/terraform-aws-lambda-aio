f55f0cae133a0583325a23cff8b15b2c09153ca7
Anthony Gargiulo <anthony@agargiulo.com>
2020-04-20 16:41:10 -0700

    Remove sleep section, update lambda to latest release

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ variable "lambda_function" {
   default = {
     controller = {
       name    = "controller"
-      version = "v0.2.2"
+      version = "v0.3.0"
       timeout = 900
       runtime = "go1.x"
     }
